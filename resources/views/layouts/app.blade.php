d37351df108ea71d92bb4007bbfb6c5a381c6f7b
Abdurhman Abrar <findabdurhman@gmail.com>
2022-05-05 15:47:44 +0300

    fix ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
     <!--begin::Header Mobile-->
     <div id="kt_header_mobile" class="header-mobile align-items-center header-mobile-fixed">
         <!--begin::Logo-->
-        <a href="">
+        <a href="/">
             <img alt="Logo" src="{{ asset('ju_logo.png') }}" width="40" />
         </a>
         <!--end::Logo-->
@@ -78,7 +78,6 @@
         </div>
         <!--end::Toolbar-->
     </div>
-
     <!--end::Header Mobile-->
     <div class="d-flex flex-column flex-root">
         <div class="flex-row d-flex flex-column-fluid page">
@@ -221,116 +220,118 @@
                     <!--end::Menu Container-->
                 </div>
                 <!--end::Aside Menu-->
-                <!--begin::Wrapper-->
-                <div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
-                    <!--begin::Header-->
-                    <div id="kt_header" class="header header-fixed">
-                        <!--begin::Container-->
-                        <div class="container-fluid d-flex align-items-stretch justify-content-between">
-                            <!--begin::Header Menu Wrapper-->
-                            <div class="header-menu-wrapper header-menu-wrapper-left" id="kt_header_menu_wrapper">
-                                <!--begin::Header Menu-->
-                                <div id="kt_header_menu"
-                                    class="header-menu header-menu-mobile header-menu-layout-default">
-                                    <!--begin::Header Nav-->
-                                    <ul class="menu-nav">
+            </div>
+            <!--begin::Wrapper-->
+            <div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
+                <!--begin::Header-->
+                <div id="kt_header" class="header header-fixed">
+                    <!--begin::Container-->
+                    <div class="container-fluid d-flex align-items-stretch justify-content-between">
+                        <!--begin::Header Menu Wrapper-->
+                        <div class="header-menu-wrapper header-menu-wrapper-left" id="kt_header_menu_wrapper">
+                            <!--begin::Header Menu-->
+                            <div id="kt_header_menu" class="header-menu header-menu-mobile header-menu-layout-default">
+                                <!--begin::Header Nav-->
+                                <ul class="menu-nav">
 
-                                    </ul>
-                                    <!--end::Header Nav-->
-                                </div>
-                                <!--end::Header Menu-->
+                                </ul>
+                                <!--end::Header Nav-->
                             </div>
-                            <!--end::Header Menu Wrapper-->
-                            <!--begin::Topbar-->
-                            <div class="topbar">
-                                <!--begin::User-->
-                                <div class="topbar-item">
-                                    <div class="w-auto px-2 btn btn-icon btn-clean d-flex align-items-center btn-lg"
-                                        id="kt_quick_user_toggle">
-                                        <span
-                                            class="mr-1 text-muted font-weight-bold font-size-base d-none d-md-inline">Hi,</span>
-                                        <span
-                                            class="mr-3 text-dark-50 font-weight-bolder font-size-base d-none d-md-inline">{{ Auth::user()->first_name }}</span>
-                                        <span class="symbol symbol-35 symbol-light-success">
-                                            <span
-                                                class="symbol-label font-size-h5 font-weight-bold">{{ Auth::user()->first_name[0] }}</span>
-                                        </span>
-                                    </div>
-                                </div>
-                                <!--end::User-->
-                            </div>
-                            <!--end::Topbar-->
+                            <!--end::Header Menu-->
                         </div>
-                        <!--end::Container-->
+                        <!--end::Header Menu Wrapper-->
+                        <!--begin::Topbar-->
+                        <div class="topbar">
+                            <!--begin::User-->
+                            <div class="topbar-item">
+                                <div class="w-auto px-2 btn btn-icon btn-clean d-flex align-items-center btn-lg"
+                                    id="kt_quick_user_toggle">
+                                    <span
+                                        class="mr-1 text-muted font-weight-bold font-size-base d-none d-md-inline">Hi,</span>
+                                    <span
+                                        class="mr-3 text-dark-50 font-weight-bolder font-size-base d-none d-md-inline">{{ Auth::user()->first_name }}</span>
+                                    <span class="symbol symbol-35 symbol-light-success">
+                                        <span
+                                            class="symbol-label font-size-h5 font-weight-bold">{{ Auth::user()->first_name[0] }}</span>
+                                    </span>
+                                </div>
+                            </div>
+                            <!--end::User-->
+                        </div>
+                        <!--end::Topbar-->
                     </div>
-                    <!--end::Header-->
-                    <!--begin::Content-->
-                    <!--begin::Subheader-->
-                    <div class="py-2 subheader py-lg-4 subheader-solid" id="kt_subheader">
-                        <div
-                            class="flex-wrap container-fluid d-flex align-items-center justify-content-between flex-sm-nowrap">
-                            <!--begin::Info-->
-                            <div class="flex-wrap mr-1 d-flex align-items-center">
-                                <!--begin::Page Heading-->
-                                <div class="mr-5 d-flex align-items-baseline">
-                                    <!--begin::Page Title-->
-                                    <h5 class="my-2 mr-5 text-dark font-weight-bold">@yield('breadcrumbTitle')</h5>
-                                    <!--end::Page Title-->
-                                    <!--begin::Breadcrumb-->
-                                    <ul
-                                        class="p-0 my-2 breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold font-size-sm">
-                                        @yield('breadcrumbList')
-                                        {{-- <li class="breadcrumb-item">
+                    <!--end::Container-->
+                </div>
+                <!--end::Header-->
+                <!--begin::Content-->
+                <!--begin::Subheader-->
+                <div class="py-2 subheader py-lg-4 subheader-solid" id="kt_subheader">
+                    <div class="flex-wrap container-fluid  align-items-center justify-content-between flex-sm-nowrap">
+                        <!--begin::Info-->
+                        <div class="flex-wrap mr-1  align-items-center">
+                            <!--begin::Page Heading-->
+                            <div class="mr-5 d-flex align-items-baseline justify-between">
+                                <!--begin::Page Title-->
+                                <h5 class="my-2 mr-5 text-dark font-weight-bold">@yield('breadcrumbTitle')</h5>
+                                <!--end::Page Title-->
+                                <!--begin::Breadcrumb-->
+                                <ul
+                                    class="p-0 my-2 ml-auto breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold font-size-sm">
+                                    <li class="breadcrumb-item">
+                                        <a href="/" class="text-muted">Home</a>
+                                    </li>
+                                    @yield('breadcrumbList')
+                                    {{-- <li class="breadcrumb-item">
                                                 <a href="" class="text-muted">General</a>
                                             </li>
                                             <li class="breadcrumb-item">
                                                 <a href="" class="text-muted">Empty Page</a>
                                             </li> --}}
-                                    </ul>
-                                    <!--end::Breadcrumb-->
-                                </div>
-                                <!--end::Page Heading-->
+                                </ul>
+                                <!--end::Breadcrumb-->
                             </div>
-                            <!--end::Info-->
+                            <!--end::Page Heading-->
                         </div>
+                        <!--end::Info-->
                     </div>
-                    <!--end::Subheader-->
-                    <!--begin::Entry-->
-                    <div class="d-flex flex-column-fluid">
-                        <!--begin::Container-->
-                        <div class="container">
-                            @yield('content')
-                        </div>
-                        <!--end::Container-->
-                    </div>
-                    <!--end::Entry-->
                 </div>
-                <!--end::Content-->
-                <!--begin::Footer-->
-                <div class="py-4 bg-white footer d-flex flex-lg-column" id="kt_footer">
+                <!--end::Subheader-->
+                <!--begin::Entry-->
+                <div class=" flex-column-fluid">
                     <!--begin::Container-->
-                    <div
-                        class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
-                        <!--begin::Copyright-->
-                        <div class="order-2 text-dark order-md-1">
-                            <span class="mr-2 text-muted font-weight-bold">2020©</span>
-                            <a href="#" target="_blank" class="text-dark-75 text-hover-primary">JU</a>
-                        </div>
-                        <!--end::Copyright-->
-                        <!--begin::Nav-->
-                        <div class="nav nav-dark">
-                            <a href="#" target="_blank" class="pl-0 pr-5 nav-link">About</a>
-                            <a href="#" target="_blank" class="pl-0 pr-5 nav-link">Team</a>
-                            <a href="#" target="_blank" class="pl-0 pr-0 nav-link">Contact</a>
-                        </div>
-                        <!--end::Nav-->
+                    <div class="container mt-6">
+                        @yield('content')
                     </div>
                     <!--end::Container-->
                 </div>
-                <!--end::Footer-->
+                <!--end::Entry-->
             </div>
+            <!--end::Content-->
         </div>
-        <!--end::Aside-->
+        <!--begin::Footer-->
+        <div class="py-4 bg-white footer d-flex flex-lg-column" id="kt_footer">
+            <!--begin::Container-->
+            <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
+                <!--begin::Copyright-->
+                <div class="order-2 text-dark order-md-1">
+                    <span class="mr-2 text-muted font-weight-bold">2020©</span>
+                    <a href="#" target="_blank" class="text-dark-75 text-hover-primary">JU</a>
+                </div>
+                <!--end::Copyright-->
+                <!--begin::Nav-->
+                <div class="nav nav-dark">
+                    <a href="#" target="_blank" class="pl-0 pr-5 nav-link">About</a>
+                    <a href="#" target="_blank" class="pl-0 pr-5 nav-link">Team</a>
+                    <a href="#" target="_blank" class="pl-0 pr-0 nav-link">Contact</a>
+                </div>
+                <!--end::Nav-->
+            </div>
+            <!--end::Container-->
+        </div>
+        <!--end::Footer-->
+    </div>
+    </div>
+    <!--end::Aside-->
     </div>
 
     <!--end::Main-->
