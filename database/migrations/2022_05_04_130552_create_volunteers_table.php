4e612256367eeba5090e62e6d866e2d0c23df4fd
Abdurhman Abrar <findabdurhman@gmail.com>
2022-05-05 07:14:41 +0300

    volenteer and status db migration

--- database/migrations/2022_05_04_130552_create_volunteers_table.php
+++ database/migrations/2022_05_04_130552_create_volunteers_table.php
@@ -15,6 +15,27 @@ return new class extends Migration
     {
         Schema::create('volunteers', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('father_name');
+            $table->string('grand_father_name');
+            $table->string('email')->unique();
+            $table->date('dob');
+            $table->string('gender');
+            $table->string('phone');
+            $table->string('contact_name');
+            $table->string('contact_phone');
+            $table->float('gpa');
+            $table->string('photo')->nullable();
+            $table->string('bsc_document');
+            $table->string('msc_document')->nullable();
+            $table->string('ministry_document');
+            $table->string('non_pregnant_validation_document')->nullable();
+            $table->string('ethical_license')->nullable();
+            $table->string('kebele_id');
+            $table->foreignId('educational_level_id')->nullable()->constrained('educational_levels','id')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreignId('field_of_study_id')->nullable()->constrained('feild_of_studies')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreignId('woreda_id')->nullable()->constrained('woredas')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreignId('disablity_id')->nullable()->constrained('disablities')->nullOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
