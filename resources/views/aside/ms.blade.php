3216c2f9fe786e4079bc6e2cd36e8fd88b117622
milkyseifu <milkyseifuban@gmail.com>
2022-05-05 06:24:45 -0700

    ms

--- resources/views/aside/ms.blade.php
+++ resources/views/aside/ms.blade.php
@@ -1,12 +1,4 @@
-<div class="menu-submenu">
-    <i class="menu-arrow"></i>
-    <ul class="menu-subnav">
-        <li class="menu-item menu-item-parent" aria-haspopup="true">
-            <span class="menu-link">
-                <span class="menu-text">Settings</span>
-            </span>
-        </li>
-        <li class="menu-item {{ strpos(Route::currentRouteName(), 'region.index') === 0 ? 'menu-item-active' : '' }}"
+<li class="menu-item {{ strpos(Route::currentRouteName(), 'region.index') === 0 ? 'menu-item-active' : '' }}"
             aria-haspopup="true">
             <a href="{{ route('region.index', []) }}" class="menu-link">
                 <i class="menu-bullet menu-bullet-dot">
@@ -33,5 +25,3 @@
                 <span class="menu-text">Woreda</span>
             </a>
         </li>
-    </ul>
-</div>
