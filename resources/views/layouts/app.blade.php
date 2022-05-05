b3393c117d51fbb109f25fe2276fa5ebcc9ff3a5
Abdurhman Abrar <findabdurhman@gmail.com>
2022-05-05 15:28:28 +0300

    layout fixing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>@stack('title') | {{ env('APP_NAME') }}</title>
+    <title>@yield('title') | {{ env('APP_NAME') }}</title>
     <meta name="description" content="Page with empty content" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -26,7 +26,7 @@
     <link href="{{ asset('assets/css/themes/layout/aside/dark.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('fontawesome/css/all.min.css') }}" rel="stylesheet">
     <!--end::Layout Themes-->
-    @stack('cssPage')
+    @stack('css')
     <link rel="shortcut icon" href="{{ asset('ju_logo.png') }}" />
 </head>
 <!--end::Head-->
@@ -274,12 +274,12 @@
                                 <!--begin::Page Heading-->
                                 <div class="mr-5 d-flex align-items-baseline">
                                     <!--begin::Page Title-->
-                                    <h5 class="my-2 mr-5 text-dark font-weight-bold">@stack('breadcrumbTitle')</h5>
+                                    <h5 class="my-2 mr-5 text-dark font-weight-bold">@yield('breadcrumbTitle')</h5>
                                     <!--end::Page Title-->
                                     <!--begin::Breadcrumb-->
                                     <ul
                                         class="p-0 my-2 breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold font-size-sm">
-                                        @stack('breadcrumbList')
+                                        @yield('breadcrumbList')
                                         {{-- <li class="breadcrumb-item">
                                                 <a href="" class="text-muted">General</a>
                                             </li>
@@ -503,7 +503,7 @@
     <!--end::Page Vendors-->
     <!--begin::Page Scripts(used by this page)-->
     <script src="{{ asset('assets/js/pages/widgets.js') }}"></script>
-    @stack('jsPage')
+    @stack('js')
     <!--end::Page Scripts-->
     <script>
         @if (Session::has('message'))
