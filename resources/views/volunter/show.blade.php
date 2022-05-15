5e2666da152c13b0fa3ca274f5828aa5af0ff0fa
Ajaib <findajaib@gmail.com>
2022-05-15 01:44:20 -0700

    ready for route chaging

--- resources/views/volunter/show.blade.php
+++ resources/views/volunter/show.blade.php
@@ -92,7 +92,7 @@
 
                                 </div>
                                 {{-- @dd($applicant->status) --}}
-                                @if ($applicant->status?->acceptance_status == 0 || $applicant->status?->acceptance_status == 2)
+                                @if ($applicant->status?->acceptance_status == 0 ||     )
                                     <form action="{{ route('applicant.screen', ['applicant_id' => $applicant->id]) }}"
                                         method="POST">
                                         @csrf
