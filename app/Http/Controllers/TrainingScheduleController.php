cc49284c971d445bcf991eee7ad0334c5c37a18b
milkyseifu <milkyseifuban@gmail.com>
2022-05-17 08:49:34 -0700

    phase two migration done

---
+++ app/Http/Controllers/TrainingScheduleController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TrainingSchedule;
+use App\Http\Requests\StoreTrainingScheduleRequest;
+use App\Http\Requests\UpdateTrainingScheduleRequest;
+
+class TrainingScheduleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTrainingScheduleRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTrainingScheduleRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TrainingSchedule  $trainingSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TrainingSchedule $trainingSchedule)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TrainingSchedule  $trainingSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TrainingSchedule $trainingSchedule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTrainingScheduleRequest  $request
+     * @param  \App\Models\TrainingSchedule  $trainingSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTrainingScheduleRequest $request, TrainingSchedule $trainingSchedule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TrainingSchedule  $trainingSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TrainingSchedule $trainingSchedule)
+    {
+        //
+    }
+}
