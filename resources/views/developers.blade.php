99ca9ef270750f7f2c84d680993fd8bf3a6e3e7c
Abdurhman Abrar <findabdurhman@gmail.com>
2023-01-04 03:10:34 -0500

    chang on dev

--- resources/views/developers.blade.php
+++ resources/views/developers.blade.php
@@ -57,7 +57,7 @@
                     <!--begin::User-->
                     <div class="mt-7">
                         <div class="symbol symbol-circle symbol-lg-75">
-                            <img src="{{ asset('user.png') }}" alt="image">
+                            <img src="{{ asset('img/hailu_img.jpg') }}" alt="image">
                         </div>
                         <div class="symbol symbol-lg-75 symbol-circle symbol-primary d-none">
                             <span class="font-size-h3 font-weight-boldest">Mr. Hailu Chemir</span>
@@ -74,13 +74,13 @@
                     <!--end::Label-->
                     <!--begin::Buttons-->
                     <div class="mt-9 mb-6">
-                        <a href="#" class="btn btn-md btn-icon btn-light-facebook btn-pill mx-2">
-                            <i class="socicon-facebook"></i>
+                        <a href="mailto:hchemir@gmail.com" class="btn btn-md btn-icon btn-light-google btn-pill mx-2">
+                            <i class="socicon-google"></i>
                         </a>
                         <a href="#" class="btn btn-md btn-icon btn-light-twitter btn-pill mx-2">
                             <i class="socicon-twitter"></i>
                         </a>
-                        <a href="#" class="btn btn-md btn-icon btn-light-linkedin btn-pill mx-2">
+                        <a href="https://et.linkedin.com/in/hailu-chemir-3b982173" target="_blank" class="btn btn-md btn-icon btn-light-linkedin btn-pill mx-2">
                             <i class="socicon-linkedin"></i>
                         </a>
                     </div>
@@ -306,6 +306,50 @@
                             <i class="socicon-twitter"></i>
                         </a>
                         <a href="https://www.linkedin.com/in/seid-mohammed-039949227/" class="btn btn-md btn-icon btn-light-linkedin btn-pill mx-2">
+                            <i class="socicon-linkedin"></i>
+                        </a>
+                    </div>
+                    <!--end::Buttons-->
+                </div>
+                <!--end::Body-->
+            </div>
+            <!--end::Card-->
+        </div>
+        <!--end::Col-->
+<!--begin::Col-->
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+            <!--begin::Card-->
+            <div class="card card-custom gutter-b card-stretch">
+                <!--begin::Body-->
+                <div class="card-body text-center pt-4">
+                    <!--end::Toolbar-->
+                    <!--begin::User-->
+                    <div class="mt-7">
+                        <div class="symbol symbol-circle symbol-lg-75">
+                            <img src="{{ asset('img/ashu_img.jpg') }}" alt="image">
+                        </div>
+                        <div class="symbol symbol-lg-75 symbol-circle symbol-primary d-none">
+                            <span class="font-size-h3 font-weight-boldest">Mr. Ashenafi Alemu</span>
+                        </div>
+                    </div>
+                    <!--end::User-->
+                    <!--begin::Name-->
+                    <div class="my-2">
+                        <a href="#" class="text-dark font-weight-bold text-hover-primary font-size-h4">Mr. Ashenafi Alemu</a>
+                    </div>
+                    <!--end::Name-->
+                    <!--begin::Label-->
+                    <span class="btn btn-text btn-light-warning btn-sm font-weight-bold">Jimma University Software Developer</span>
+                    <!--end::Label-->
+                    <!--begin::Buttons-->
+                    <div class="mt-9 mb-6">
+                        <a href="mailto:ashenafialemu66@gmail.com" target="_blank" class="btn btn-md btn-icon btn-light-google btn-pill mx-2">
+                            <i class="socicon-google"></i>
+                        </a>
+                        <a href="https://twitter.com/Ashenafi__Alemu" target="_blank" class="btn btn-md btn-icon btn-light-twitter btn-pill mx-2">
+                            <i class="socicon-twitter"></i>
+                        </a>
+                        <a href="http://linkedin.com/in/ashenafi-alemu-627b44155" target="_blank" class="btn btn-md btn-icon btn-light-linkedin btn-pill mx-2">
                             <i class="socicon-linkedin"></i>
                         </a>
                     </div>
