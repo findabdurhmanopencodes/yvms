d8ce461708494c8d213190d64f3bf8f60c3e9ae4
User <milkyseifuban@gmail.com>
2023-02-06 01:56:26 +0300

    ini set

--- app/Console/Commands/TrainingPlacementCommand.php
+++ app/Console/Commands/TrainingPlacementCommand.php
@@ -84,6 +84,8 @@ class TrainingPlacementCommand extends Command
 
     public function place($trainingSessionId)
     {
+        $this->stop();
+        dd('dfd');
         $activeSession = TrainingSession::find($trainingSessionId);
         $errorMessage = null;
 
